e8fac8d33a7955bb8e4fbce308475735bd570ded
Michael Berrill <mdberrill@yahoo.co.uk>
2016-10-31 21:40:34 +0000

    Added console app and ability for the life coach to meet different people and resume their meetings.

---
+++ LifeCoach.Console/Program.cs
@@ -0,0 +1,70 @@
+﻿using CommandLine;
+using System;
+
+namespace LifeCoach.Console
+{
+    class Program
+    {
+        [Verb("status")]
+        class StatusOptions
+        {
+
+        }
+
+        [Verb("new", HelpText = "New activities")]
+        class NewOptions
+        {
+            //normal options here
+            [Option('a', "activity")]
+            public string ActivityName { get; set; }
+        }
+
+        [Verb("complete", HelpText = "complete activities")]
+        class RemoveOptions
+        {
+            //normal options here
+        }
+
+        [Verb("meet", HelpText = "Creat a meeting with the life coach. The life coach will record everything that happens in the meeting.")]
+        class MeetOptions
+        {
+            [Value(0, Required = true)]
+            public string ClientName { get; set; }
+
+            [Value(1)]
+            public string TooManyArgs { get; set; }
+        }
+
+        static int Main(string[] args)
+        {
+
+            Domain.LifeCoachRepository repo = new Domain.LifeCoachRepository();
+            Domain.LifeCoach lifeCoach = repo.LoadPreviousClient();
+            if (lifeCoach == null)
+                lifeCoach = new Domain.LifeCoach();
+
+            return Parser.Default.ParseArguments<NewOptions, RemoveOptions, StatusOptions, MeetOptions>(args)
+              .MapResult(
+                (NewOptions opts) => { System.Console.WriteLine("add " + opts.ActivityName); return 0; },
+                (RemoveOptions opts) => { System.Console.WriteLine("Remove"); return 0; },
+                (StatusOptions opts) => { System.Console.WriteLine("Status:");
+                    if (lifeCoach.CurrentClient != null)
+                        System.Console.WriteLine(" Currently meeting with " + lifeCoach.CurrentClient.Name);
+                    else
+                        System.Console.WriteLine(" No meeting");
+                    return 0; },
+                (MeetOptions opts) => {
+                    // if we have met before lets carry on where we left off
+                    var client = repo.LoadPreviousClient(opts.ClientName);
+                    if (client != null)
+                        lifeCoach.ResumeMeetingWith(client);
+                    else
+                        lifeCoach.MeetWithNewClient(opts.ClientName);
+
+                    repo.SaveClient(lifeCoach);
+                    System.Console.WriteLine("Meeting with "+lifeCoach.CurrentClient.Name); return 0; },
+                errs => 1);
+        }
+    }
+
+}
--- LifeCoach.Domain/LifeCoach.cs
+++ LifeCoach.Domain/LifeCoach.cs
@@ -1,6 +1,87 @@
-﻿namespace LifeCoach.Domain
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace LifeCoach.Domain
 {
     public class LifeCoach
     {
+        public Client CurrentClient { get; set; }
+
+        public Client MeetWithNewClient(string clientName)
+        {
+            CurrentClient = new Client(clientName);
+            return CurrentClient;
+        }
+
+        public Client GetCurrentClientMeetingWith()
+        {
+            return CurrentClient;
+        }
+
+        public void ResumeMeetingWith(Client client)
+        {
+            CurrentClient = client;
+        }
     }
+
+    public class LifeCoachRepository
+    {
+
+        public LifeCoach LoadPreviousClient()
+        {
+            XmlSerializer serialiser = new XmlSerializer(typeof(LifeCoach));
+            if (File.Exists(".lifecoach/lifecoach.xml"))
+            {
+                using (StreamReader sr = new StreamReader(".lifecoach/lifecoach.xml"))
+                using (XmlReader reader = XmlReader.Create(sr))
+                {
+                    return serialiser.Deserialize(reader) as LifeCoach;
+                }
+            }
+            else return null;
+        }
+
+        public void SaveClient(LifeCoach lifeCoach)
+        {
+            XmlSerializer serialiser = new XmlSerializer(typeof(LifeCoach));
+            if (!Directory.Exists(".lifecoach"))
+                Directory.CreateDirectory(".lifecoach");
+
+            using (StreamWriter sw = new StreamWriter(".lifecoach/lifecoach.xml"))
+            using (XmlWriter writer = XmlWriter.Create(sw))
+            {
+                serialiser.Serialize(writer, lifeCoach);
+                writer.Close();
+                sw.Close();
+            }
+            if (lifeCoach.CurrentClient == null) return;
+            // save client in file too
+            XmlSerializer clientSerialiser = new XmlSerializer(typeof(Client));
+            using (StreamWriter sw = new StreamWriter(".lifecoach/" + lifeCoach.CurrentClient.Name + ".xml"))
+            using (XmlWriter writer = XmlWriter.Create(sw))
+            {
+                clientSerialiser.Serialize(writer, lifeCoach.CurrentClient);
+                writer.Close();
+                sw.Close();
+            }
+        }
+
+        public Client LoadPreviousClient(string clientName)
+        {
+            XmlSerializer clientSerialiser = new XmlSerializer(typeof(Client));
+            if (File.Exists(".lifecoach/" + clientName + ".xml"))
+            {
+                using (StreamReader sr = new StreamReader(".lifecoach/" + clientName + ".xml"))
+                using (XmlReader reader = XmlReader.Create(sr))
+                {
+                    return clientSerialiser.Deserialize(reader) as Client;
+                }
+            }
+            else return null;
+        }
+    }
+
+
 }
